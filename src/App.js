079c5dc28b8ba457282774d9a963f2c510ae9b06
jonbaxt <jonbaxt@gmail.com>
2018-08-17 23:31:16 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <section className='topArea' >
+          <p>What's up bitches</p>
+        </section>
+          To get started, edit <code>src/App.js</code> and save to reload.
+      </div>
+    );
+  }
+}
+
+export default App;
